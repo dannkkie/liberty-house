c05167e1af582c5902de4697b2d613de3f06fc12
Eddie <ekkpodogui@gmail.com>
2019-05-28 10:32:25 +0000

    removed API Keys

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,7 +7,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyDjwkvBuXxmYm9aUCOYJ9Pl5pis89qSVmY")
+    GMSServices.provideAPIKey("API KEY HERE")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
